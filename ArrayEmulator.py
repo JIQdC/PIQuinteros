002f6ad7e8d676f52dddc2878c7997465a639684
Jose Ignacio <josequinterosdelcastillo@gmail.com>
2019-08-12 16:39:15 -0300

    Nueva frec. central, corrección de acumulador, def. de longitud inicial.

--- ArrayEmulator.py
+++ ArrayEmulator.py
@@ -5,10 +5,10 @@ c=299792458 #velocidad de la luz
 MHz=1E6
 
 #parámetros iniciales
-f=32*MHz                #frecuencia de la onda propagante
-theta=np.radians(30)    #ángulo en radianes
-vp=0.72*c               #velocidad de propagación (como fracción de c)
-N=8                     #cantidad de sensores
+f=150*MHz                #frecuencia de la onda propagante
+theta=np.radians(15)    #ángulo en radianes
+vp=0.66*c               #velocidad de propagación (como fracción de c)
+N=16                    #cantidad de sensores
 d=0.5*c/f               #separación entre sensores (como fracción de long de onda en aire)
 
 #diferencia de tiempo de vuelo en aire
@@ -16,13 +16,14 @@ tau=d*np.sin(theta)/c
 
 #parámetros del cable
 wavelength=vp/f         #longitud de onda en el cable
-li=10*wavelength        #longitud inicial (base de todos los tramos de cable)
+li=0.2                  #longitud inicial (base de todos los tramos de cable)
 dl=vp*tau               #equivalente en longitud de cable para el tiempo de vuelo en aire
 
 #ahora sumamos los N tramos de cable, cada uno agregando un dl
 L=0                     #longitud de cable total
-for n in range(1,N):
-    L=li+n*dl
+for n in range(0,N-1):
+    L=L+li+n*dl
 
+print("Longitud de onda en el cable: ",wavelength,"m")
 print("Longitud total de cable: ",L,"m")
 print("Longitud de retardo en cable: ",dl,"m")
