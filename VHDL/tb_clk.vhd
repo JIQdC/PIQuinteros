5b7f558c81f80f3cb5d9809733a01d40fdb493b0
Jose Ignacio <josequinterosdelcastillo@gmail.com>
2019-09-27 16:25:55 -0300

    Creación de carpeta /VHDL para guardar los fuentes que vaya desarrollando. Agregué archivos fuentes para la simulación de transacciones de bus AXI, utilizando simuladores de CLOCK y RST_N. Script TCL para recrear el proyecto de Vivado que estoy usando.

---
+++ VHDL/tb_clk.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company:  Instituto Balseiro
+-- Engineer: Guillermo Guichal
+-- 
+-- Design Name: 
+-- Module Name: 
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: Ejemplos b�sicos de dise�o VHDL
+-- 
+-- Dependencies: None.
+-- 
+-- Revision: 2016-02-18.01
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_clk is
+    generic(T_SIM_LIMIT  : time := 10 us;
+            T_CLK_PERIOD : time := 10 ns);
+    port ( clk_o  : out std_logic := '0');
+end tb_clk;
+
+architecture sim of tb_clk is
+
+    constant T_CLK_HIGH   : time := T_CLK_PERIOD/2;
+
+begin
+
+    process
+    begin
+        clk_o <= '0';
+
+        while (now < T_SIM_LIMIT) loop
+            wait for T_CLK_PERIOD - T_CLK_HIGH;
+            clk_o <= '1';
+            wait for T_CLK_HIGH; 
+            clk_o <= '0';
+        end loop;
+        report "Fin de simulación.";
+        wait;
+    end process;    
+      
+end sim;
