f4586689e06325aa711a718ad9ca19e04769df45
Jose Ignacio <josequinterosdelcastillo@gmail.com>
2019-10-21 20:10:51 -0300

    Correción de tiempos de duración de señales de clock interno y data clock. Corrección de contador. Sincronización general de señales. Adición de flag usr_ready para su utilización en etapas posteriores de pruebas. Chequeo de estilo.

--- VHDL/adc_model.vhd
+++ VHDL/adc_model.vhd
@@ -11,151 +11,135 @@
 -- 
 -- Dependencies: None.
 -- 
--- Revision: 2019-10-11
+-- Revision: 2019-10-21
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 
 entity ADC_model is
     generic(
         N: integer := 14; --cantidad de bits del ADC
-        t_sample : time := 7.69 ns; --tiempo en nanosegundos
-        t_data : time := 549.29 ps;
-        t_delay : time := 274.64 ps
-        --t_FCO : real := 2.3 ns --propagación hasta frame
+        T_SAMPLE : time := 14 ns; --período de muestreo del ADC
+        T_DELAY : time := 2.3 ns --tiempo de propagación entre muestreo y dato
     );
     port(
         rst_n: in std_logic;
         d_in: in std_logic_vector((N-1) downto 0);
-        DCO: out std_logic;
-        FCO: out std_logic;
-        DATA: out std_logic
+        usr_ready: out std_logic; --flag que indica si ya se enganchó con adc_FCO
+        adc_DCO: out std_logic;
+        adc_FCO: out std_logic;
+        adc_D: out std_logic
     );
 end ADC_model;
 
 architecture arch of ADC_model is
     --variables para los tiempos de los clocks
-    constant nseg: time := 1 ns;
-    --signal t_data, t_sample, t_delay: time;
-    --signal t_data_real, N_real, t_delay_real: real;
+    constant T_BIT: time := T_SAMPLE/N;
     --registro para almacenar el dato que entra en paralelo
-    signal data_reg, data_next: std_logic_vector((N-1) downto 0) := (others => '0');
+    signal adc_D_reg, adc_D_next: std_logic_vector((N-1) downto 0) := (others => '0');
     --señales auxiliares para los clocks internos
-    signal clk_int,FCO_int,DCO_int,DATA_int: std_logic := '0';
+    signal clk_int,adc_FCO_int,adc_DCO_int,adc_D_int: std_logic := '0';
     --registro de estado
-	type state_type is (capture, tx);
+	type state_type is (idle,capture, tx);
     signal state_reg,state_next: state_type;
     --contador
     signal cont_reg,cont_next: integer := 0;
 
 begin
-    --convert: process
-    --begin
-    --    N_real <= real(N);
-    --    t_data_real <= t_sample_real/N_real;
-    --    t_delay_real <= t_sample_real/(real(2)*N_real);
-    --    t_data <= t_data_real * nseg;--tiempo de bit
-    --    t_sample <= t_sample_real * nseg; --tiempo de muestra
-    --    t_delay <= t_delay_real * nseg;
-    --    wait;
-    --end process;
-
-    -- clock interno al doble de la tasa de datos
+    -- clock interno a la tasa de datos
     clk_int_process: process
     begin
             clk_int <= '1';
-            wait for t_data/4;
+            wait for T_BIT/2;
             clk_int <= '0';
-            wait for t_data/4;
+            wait for T_BIT/2;
     end process;
 
     --Proceso para generar la señal de frame
-    FCO_int_process: process
+    adc_FCO_int_process: process
     begin
-            FCO_int <= '1';
-            wait for t_sample/2;
-            FCO_int <= '0';
-            wait for t_sample/2;
+            adc_FCO_int <= '1';
+            wait for T_SAMPLE/2;
+            adc_FCO_int <= '0';
+            wait for T_SAMPLE/2;
     end process;
     
-    -- DCO interno a la tasa de datos
-    DCO_int_process: process
+    -- adc_DCO interno a la mitad de la tasa de datos (porque levanto datos en ambos flancos)
+    adc_DCO_int_process: process
     begin
-            DCO_int <= '1';
-            wait for t_data/2;
-            DCO_int <= '0';
-            wait for t_data/2;
+            adc_DCO_int <= '1';
+            wait for T_BIT;
+            adc_DCO_int <= '0';
+            wait for T_BIT;
     end process;    
 
     --state register
     process(clk_int,rst_n)
     begin
         if (rst_n='0') then
-            --el estado on reset del modelo es capturar, de esta forma, debería sincronizarse
-            --todo con los relojes (porque empieza capturando)
-            state_reg <= capture;
-            data_reg <= (others=>'0');
+            --el estado on reset del modelo es idle (espera a adc_FCO)
+            state_reg <= idle;
+            adc_D_reg <= (others=>'0');
             cont_reg <= 0;
         elsif(rising_edge(clk_int)) then
             state_reg <= state_next;
-            data_reg <= data_next;
+            adc_D_reg <= adc_D_next;
             cont_reg <= cont_next;
         end if;
     end process;
 
     --next state logic
-    process(data_reg,state_reg,cont_reg,FCO_int,d_in)
+    process(adc_D_reg,state_reg,cont_reg,adc_FCO_int,d_in)
     begin
         --default
-        data_next <= data_reg;
+        adc_D_next <= adc_D_reg;
         state_next <= state_reg;
 
         case state_reg is
+            when idle =>
+                --este estado debería ocurrir solo una vez, para engancharse con adc_FCO
+                if(rising_edge(adc_FCO_int)) then
+                    cont_next <= N-1;
+                    adc_D_next <= d_in;
+                    state_next <= tx;
+                end if;
+                --sino, queda en idle            
             when capture =>
                 cont_next <= N-1;
-                data_next <= d_in;
+                adc_D_next <= d_in;
                 state_next <= tx;
             when tx =>
-                if (cont_reg > 0) then
+                if (cont_reg > 1) then
                     cont_next <= cont_reg - 1;
                     state_next <= tx;
                 else
+                    cont_next <= cont_reg - 1;
                     state_next <= capture;
                 end if;
         end case;
     end process;
 
-    --output logic
-    process(data_reg,state_reg)
+    --la salida siempre es...
+    adc_D_int <= adc_D_reg(cont_reg);
+    --flag que indica cuándo se enganchó con adc_FCO
+    process(state_reg)
     begin
-        --default
-        DATA_int <= '0';
-
         case state_reg is
-            when capture =>
-                DATA_int <= data_reg(cont_reg);
-            when tx =>
-                DATA_int <= data_reg(cont_reg);
+            when idle =>
+                usr_ready <= '0';
+            when others =>
+                usr_ready <= '1';
         end case;
     end process;
 
-    --delays para acomodar todo
-    DATA <= DATA_int;
-    FCO <= FCO_int;
-    DCO <= DCO_int;     
-
+    --delays para acomodar todo. todas las señales de salida tienen T_DELAY con respecto a la entrada
+    --adc_FCO y adc_DCO se retardan T_BIT adicional para coincidir con el flujo de datos
+    adc_D <= transport adc_D_int after T_DELAY;
+    adc_FCO <= transport adc_FCO_int after T_BIT+T_DELAY;
+    adc_DCO <= transport adc_DCO_int after T_BIT+T_DELAY+T_BIT/2; --retardo de T_BIT/2 para que los flancos queden en el centro de los datos
    
 end arch; -- arch
--- VHDL/test_adc_model.vhd
+++ VHDL/test_adc_model.vhd
@@ -2,18 +2,16 @@
 -- Company: IB
 -- Engineer: José Quinteros
 -- 
--- Create Date: 09/20/2019 03:33:59 PM
 -- Design Name: 
--- Module Name: TestAXI - Behavioral
+-- Module Name:
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
--- Description: Testbench para transacciones AXI con dos slaves
+-- Description: Testbench para modelo de conversor AD9249
 -- 
 -- Dependencies: 
 -- 
--- Revision:
--- Revision 0.01 - File Created
+-- Revision: 2019-10-21
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
@@ -32,26 +30,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity TestADCmodel is
-    --  no ports - this is a testbench;
+        --  no ports - this is a testbench;
 end TestADCmodel;
     
 architecture sim of TestADCmodel is
 
-    signal clk, rst_n : std_logic;
-    signal data_in: std_logic_vector(13 downto 0) := "10101011101010";
-    signal DCO, FCO, DATA: std_logic := '0';
-    
-    begin
-    
-    U1: entity work.tb_clk(sim)
-            port map(clk_o => clk);
+        signal clk, rst_n : std_logic;
+        signal data_in: std_logic_vector(13 downto 0) := "11101010101000";
+        signal adc_DCO, adc_FCO, adc_D, usr_ready: std_logic := '0';
 
-    U2: entity work.tb_rst(sim)
-            generic map(RST_ACTIVO => '0', T_RST_INICIO => 0 ns, T_RST_ACTIVO => 60 ns)
-            port map(rst_async_o => open, rst_sync_o => rst_n, clk_i => clk);
+begin
+        U1: entity work.tb_clk(sim)
+                generic map(T_SIM_LIMIT => 10 us, T_CLK_PERIOD => 15 ns)
+                port map(clk_o => clk);
 
-    DUT: entity work.ADC_model(arch)
-            port map(rst_n => rst_n, d_in => data_in, DCO => DCO, FCO => FCO, DATA => DATA);
-        
-        
+        U2: entity work.tb_rst(sim)
+                generic map(RST_ACTIVO => '0', T_RST_INICIO => 0 ns, T_RST_ACTIVO => 27 ns)
+                port map(rst_async_o => open, rst_sync_o => rst_n, clk_i => clk);
+
+        DUT: entity work.ADC_model(arch)
+                generic map(N => 14, T_SAMPLE => 14 ns, T_DELAY => 2.3 ns)
+                port map(rst_n => rst_n, d_in => data_in, adc_DCO => adc_DCO, adc_FCO => adc_FCO, adc_D => adc_D,
+                usr_ready => usr_ready);
+
 end sim;
