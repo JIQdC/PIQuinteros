0adb481f275291c9e3f96953df88fc5d1fd2d364
José Ignacio QdC <josequinterosdelcastillo@gmail.com>
2020-06-10 19:03:30 -0300

    Actualización de lógica de deserializer a partir de medición de señales de salida de SelectIO

--- VHDL/deserializer.vhd
+++ VHDL/deserializer.vhd
@@ -51,7 +51,7 @@ architecture arch of deserializer is
 
         process(d_reg,f_reg,d_in,d_frame)
         begin
-            d_next <= d_in(1) & d_in(0) & d_reg((N-3) downto 0);
+            d_next <= d_reg((N-3) downto 0) & d_in(1) & d_in(0);
             f_next <= d_frame;
             if(f_reg = '0' and d_frame = '1') then --rising edge del frame
                 d_out <= d_reg;
