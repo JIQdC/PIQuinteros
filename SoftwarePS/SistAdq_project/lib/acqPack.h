a562f4a9f9d4c7e057b842cad130e135a7ec1360
José Ignacio QdC <josequinterosdelcastillo@gmail.com>
2020-09-16 18:19:53 -0300

    Redefinicion de PACK_SIZE en funcion de PROG_FULL level. Nuevos campos para timestamp y para almacenar valor de clk_divider.

--- SoftwarePS/SistAdq_project/lib/acqPack.h
+++ SoftwarePS/SistAdq_project/lib/acqPack.h
@@ -4,23 +4,26 @@
 #include <time.h>
 #include <stdint.h>
 
-#define PACK_SIZE (1<<13)
+//PACK SIZE MUST BE EQUAL TO PROG_FULL LEVEL/2 IN FIFO
+#define PACK_SIZE 5000
 
 typedef struct __attribute__ ((packed))
 {
-    struct timespec acq_timestamp;
+    uint64_t acq_timestamp_sec;
+    uint64_t acq_timestamp_nsec;
     uint8_t bd_id;
     uint8_t ch_id;
     uint16_t ch_adc;
+    uint8_t clk_divider;
     uint32_t fifo_flags;
     uint16_t payload_size;
 }AcqPack_Header_t;
 
 
+// typedef struct __attribute__ ((packed))
 typedef struct __attribute__ ((packed))
-{    
+{
     AcqPack_Header_t header;
-    //uint32_t flags[PACK_SIZE];
     uint32_t data[PACK_SIZE];
 }AcqPack_t;
 
