e71cebdbbcf38f2c2f8b3207f9f5d96403f1bebe
José Ignacio QdC <josequinterosdelcastillo@gmail.com>
2020-09-29 19:13:19 -0300

    Función para convertir enteros a base arbitraria

---
+++ SoftwarePS/SistAdq_project/lib/intToBase.c
@@ -0,0 +1,15 @@
+#include "intToBase.h"
+
+//converts int to string in specified base
+char* intToBase(int val, int base){
+	
+	static char buf[32] = {0};
+	
+	int i = 30;
+	
+	for(; val && i ; --i, val /= base)
+	
+		buf[i] = "0123456789abcdef"[val % base];
+	
+	return &buf[i+1];
+}
---
+++ SoftwarePS/SistAdq_project/lib/intToBase.h
@@ -0,0 +1,7 @@
+#ifndef INTTOBASE_H_
+#define INTTOBASE_H_
+
+//converts int to string in specified base
+char* intToBase(int val, int base);
+
+#endif //INTTOBASE_H_
