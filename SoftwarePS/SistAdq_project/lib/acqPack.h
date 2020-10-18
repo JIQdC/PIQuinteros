ad4adf6c609485e12c9aa6c0a60edcb58b99966d
José Ignacio QdC <josequinterosdelcastillo@gmail.com>
2020-10-18 19:36:08 -0300

    Adaptacion a 16 canales. Utilizacion de union para escribir en uint32_t y leer en uint16_t

--- SoftwarePS/SistAdq_project/lib/acqPack.h
+++ SoftwarePS/SistAdq_project/lib/acqPack.h
@@ -1,3 +1,14 @@
+/*
+Emulation, acquisition and data processing system for sensor matrices
+José Quinteros del Castillo
+Instituto Balseiro
+---
+Data packaging format
+
+Version: 2020-10-18
+Comments:
+*/
+
 #ifndef ACQPACK_H_
 #define ACQPACK_H_
 
@@ -10,7 +21,7 @@
 #define WORD0_MASK ((1 << 14)-1)
 #define WORD1_MASK (WORD0_MASK << 14)
 
-typedef struct __attribute__ ((packed))
+typedef struct __attribute__((packed))
 {
     uint64_t acq_timestamp_sec;
     uint64_t acq_timestamp_nsec;
@@ -18,14 +29,21 @@ typedef struct __attribute__ ((packed))
     uint8_t ch_id;
     uint16_t ch_adc;
     uint8_t clk_divider;
-    uint32_t fifo_flags;
+    uint32_t fifo_flags[16];
     uint16_t payload_size;
 }AcqPack_Header_t;
 
-typedef struct __attribute__ ((packed))
+typedef union
+{
+    uint32_t data;
+    uint16_t data16[2];
+}fifo_data_t;
+
+
+typedef struct __attribute__((packed))
 {
     AcqPack_Header_t header;
-    uint32_t data[CHDATA_SIZE];
+    fifo_data_t data[16][CHDATA_SIZE];
 }AcqPack_t;
 
 #endif //ACQPACK_H_
