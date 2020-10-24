e3e1bd2555714781f10da2144102512ae2d25695
José Ignacio QdC <josequinterosdelcastillo@gmail.com>
2020-10-24 12:50:51 -0300

    Modificación para 16 canales

--- SoftwarePS/fifo_flagreader.c
+++ SoftwarePS/fifo_flagreader.c
@@ -1,17 +1,36 @@
-#include "src/CIAASistAdq.h"
+/*
+Emulation, acquisition and data processing system for sensor matrices
+José Quinteros del Castillo
+Instituto Balseiro
+---
+FIFO flag reader script for specified data channel
 
-// // Programa principal
-int main(int argc, char *argv[]) 
-{   
+Version: 2020-10-24
+Comments:
+*/
+
+#include "src/AXI_control.h"
+
+int main(int argc, char* argv[])
+{
+    uint8_t adc_ch;
+    if (argc != 2)
+    {
+        printf("usage: %s adc_ch\n", argv[0]);
+        exit(1);
+    }
+
+    sscanf(argv[1], "%hhd", &adc_ch);
+
     uint32_t rd_data;
 
-    memread(AXI_BASE_ADDR+FIFOFLAGS_ADDR,&rd_data,1);
+    memread(DATA_BASE_ADDR+FIFOFLAGS_OFF+4*adc_ch, &rd_data, 1);
 
-    fifo_flags_t *flags = malloc(sizeof(fifo_flags_t));
-    memset(flags,0,sizeof(fifo_flags_t));
-    fifoflags_reg_to_struct(flags,&rd_data);
+    fifo_flags_t* flags = malloc(sizeof(fifo_flags_t));
+    memset(flags, 0, sizeof(fifo_flags_t));
+    fifoflags_reg_to_struct(flags, &rd_data);
     print_fifo_flags(flags);
     free(flags);
 
-	return 0;
+    return 0;
 }
